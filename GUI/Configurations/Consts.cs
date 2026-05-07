82ff28db1d3c828644b2429fd4e1d73723296fa1
Tony <guerra.anthony122@gmail.com>
2026-05-06 23:11:30 -0300

    Implementando o versionamento

---
+++ GUI/Configurations/Consts.cs
@@ -0,0 +1,14 @@
+﻿using System.Text.Json;
+
+namespace GUI.Configurations;
+
+public static class Consts
+{
+    public static readonly JsonSerializerOptions JSON_SERIALIZER_OPTIONS = new()
+    {
+        PropertyNameCaseInsensitive = true
+    };
+
+    public const string UPDATE_URL =
+        "https://github.com/TonyGuerra122/NetYtDownloader/releases/latest/download/update.json";
+}
---
+++ GUI/Update/AutoUpdater.cs
@@ -0,0 +1,56 @@
+﻿using System.Net.Http;
+using System.Reflection;
+using System.Text.Json;
+using GUI.Configurations;
+using System.Diagnostics;
+using System.IO;
+
+namespace GUI.Update;
+
+public static class AutoUpdater
+{
+    public static async Task CheckForUpdateAsync()
+    {
+        using var http = new HttpClient();
+
+        string json = await http.GetStringAsync(Consts.UPDATE_URL);
+
+        var info = JsonSerializer.Deserialize<UpdateInfo>(json, Consts.JSON_SERIALIZER_OPTIONS);
+
+        if (info is null) return;
+
+        string version = Assembly
+            .GetExecutingAssembly()
+            .GetCustomAttribute<AssemblyInformationalVersionAttribute>()
+            ?.InformationalVersion ?? "0.0.1";
+
+        var currentVersion = new Version(version);
+        var latestVersion = new Version(info.Version);
+
+        if (currentVersion <= latestVersion) return;
+
+        string tempMsi = Path.Combine(
+            Path.GetTempPath(),
+            $"NetYtDownloader-{info.Version}.msi"
+        );
+
+        byte[] bytes = await http.GetByteArrayAsync(info.Url);
+        await File.WriteAllBytesAsync(tempMsi, bytes);
+
+        Process.Start(new ProcessStartInfo
+        {
+            FileName = "msiexec.exe",
+            Arguments = $"/i \"{tempMsi}\"",
+            UseShellExecute = true,
+            Verb = "runas"
+        });
+
+        Environment.Exit(0);
+    }
+
+    private class UpdateInfo
+    {
+        public string Version { get; set; } = string.Empty;
+        public string Url { get; set; } = string.Empty;
+    }
+}
